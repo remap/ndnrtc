0a2da2000aa50500de4eb8b844c78449bafc8e6e
peetonn <gpeetonn@gmail.com>
2015-07-27 00:20:58 -0700

    added helper calls for constructing ndn names

--- cpp/include/name-components.h
+++ cpp/include/name-components.h
@@ -31,6 +31,21 @@ namespace ndnrtc {
         static const std::string NameComponentFrameSegmentParity;
         static const std::string KeyComponent;
         static const std::string CertificateComponent;
+        
+        static std::string
+        getUserPrefix(const std::string& username,
+                      const std::string& prefix);
+        
+        static std::string
+        getStreamPrefix(const std::string& streamName,
+                        const std::string& username,
+                        const std::string& prefix);
+        
+        static std::string
+        getThreadPrefix(const std::string& threadName,
+                        const std::string& streamName,
+                        const std::string& username,
+                        const std::string& prefix);
     };
 }
 
