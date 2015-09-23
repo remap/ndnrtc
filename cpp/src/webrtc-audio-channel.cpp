17178f44066703864f2ea43971dca36e4bc6fda1
Jc <jcwanggm@yahoo.com>
2015-09-22 17:42:22 -0700

    add high quality audio codec(hard coded)

--- cpp/src/webrtc-audio-channel.cpp
+++ cpp/src/webrtc-audio-channel.cpp
@@ -22,6 +22,7 @@ using namespace ndnlog::new_api;
 #pragma mark - construction/destruction
 WebrtcAudioChannel::WebrtcAudioChannel():
 voeBase_(nullptr),
+voeCodec_(nullptr),
 voeNetwork_(nullptr)
 {
 }
@@ -30,6 +31,9 @@ WebrtcAudioChannel::~WebrtcAudioChannel()
 {
     if (voeBase_)
         voeBase_->Release();
+
+    if (voeCodec_)
+        voeCodec_->Release();
     
     if (voeNetwork_)
         voeNetwork_->Release();
@@ -44,7 +48,25 @@ int WebrtcAudioChannel::init()
                                        [this, &res](){
                                            voeBase_ = VoEBase::GetInterface(NdnRtcUtils::sharedVoiceEngine());
                                            voeNetwork_ = VoENetwork::GetInterface(NdnRtcUtils::sharedVoiceEngine());
+                                           //add high quality voice parameters (hard coded this time)
+                                           voeCodec_ = VoECodec::GetInterface(NdnRtcUtils::sharedVoiceEngine());
+                                           //set parameters  
+                                           /*strcpy(cinst.plname, "ISAC");  
+                                           cinst.plfreq   = 16000; // iSAC wideband sample rate
+                                           cinst.pltype   = 105;   
+                                           cinst.pacsize  = 480;   //use 30ms packet size，480kbps  
+                                           cinst.channels = 2;     // stereo  */
+                                           strcpy(cinst.plname, "opus");  //{120, "opus", 48000, 960, 2, 64000},
+                                           cinst.plfreq   = 48000;
+                                           cinst.pltype   = 120;   
+                                           cinst.pacsize  = 960; 
+                                           cinst.channels = 2; 
+                                           //cinst.rate     = -1;    // network condition self-adapt
+                                           cinst.rate     = 64000;
                                            webrtcChannelId_ = voeBase_->CreateChannel();
+                                           //tell codec parameters
+                                           voeCodec_->SetSendCodec(webrtcChannelId_, cinst);
+
                                            if (webrtcChannelId_ < 0)
                                                res = RESULT_ERR;
                                        });
