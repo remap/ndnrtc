72331553115d49c991afaebdecae88102f7ce87d
peetonn <gpeetonn@gmail.com>
2016-04-16 21:37:06 -0700

    refactored video thread class
    - simplified video thread class interface. it now allows simple use of
    boost::async in order to dispatch encoding tasks on different threads
    using system scheduler

--- cpp/src/video-thread.h
+++ cpp/src/video-thread.h
@@ -11,138 +11,50 @@
 #ifndef __ndnrtc__video_thread__
 #define __ndnrtc__video_thread__
 
-#include <boost/thread/mutex.hpp>
+#include <boost/shared_ptr.hpp>
 
 #include "video-coder.h"
-#include "media-thread.h"
-#include "threading-capability.h"
+#include "frame-data.h"
 
 namespace ndnrtc
 {
     namespace new_api
     {
-        class IVideoThreadCallback
+        class VideoThread : public NdnRtcComponent,
+                            public IEncoderDelegate
         {
         public:
-            virtual void onFrameDropped(const std::string& threadPrefix) = 0;
-            virtual void onFrameEncoded(const std::string& threadPrefix,
-                                        const FrameNumber& frameNo,
-                                        bool isKey) = 0;
-            virtual ThreadSyncList
-            getFrameSyncList(bool isKey) = 0;
-        };
-        
-        class VideoThreadSettings : public MediaThreadSettings
-        {
-        public:
-            bool useFec_;
-            IVideoThreadCallback* threadCallback_;
-            
-            VideoThreadParams*
-            getVideoParams() const
-            { return (VideoThreadParams*)threadParams_;}
-        };
-        
-        class VideoThreadStatistics : public MediaThreadStatistics
-        {
-        public:
-            VideoCoderStatistics coderStatistics_;
-            unsigned int nKeyFramesPublished_, nDeltaFramesPublished_;
-            double deltaAvgSegNum_, keyAvgSegNum_;
-            double deltaAvgParitySegNum_, keyAvgParitySegNum_;
-        };
-        
-        /**
-         * This class is a subclass of MediaThread for video streams
-         */
-        class VideoThread : public MediaThread,
-                            public IRawFrameConsumer,
-                            public IEncoderDelegate,
-                            public ThreadingCapability
-        {
-        public:
-            VideoThread();
+            VideoThread(const VideoCoderParams& coderParams);
             ~VideoThread();
             
-            static const double ParityRatio;
-            
-            // overriden from base class
-            int
-            init(const VideoThreadSettings& settings);
-            
-            void
-            stop();
-            
-            void
-            getStatistics(VideoThreadStatistics& statistics);
-            
-            void
-            getSettings(VideoThreadSettings& settings)
-            {
-                coder_->getSettings(getSettings().getVideoParams()->coderParams_);
-                settings = getSettings();
-            }
-            
-            // interface conformance
-            void
-            onDeliverFrame(WebRtcVideoFrame &frame,
-                           double unixTimeStamp);
-            
+            boost::shared_ptr<VideoFramePacket> encode(const WebRtcVideoFrame& frame);
+
             void
             setLogger(ndnlog::new_api::Logger *logger);
-            
-            bool
-            isThreadStatReady()
-            { return keyFrameNo_ > 0 && deltaFrameNo_ > 0; }
-            
+
+            unsigned int 
+            getEncodedNum() { return nEncoded_; }
+
+            unsigned int
+            getDroppedNum() { return nDropped_; }
+
         private:
-            int keyFrameNo_ = 0, deltaFrameNo_ = 0, gopCount_ = 0;
-            unsigned int deltaSegnumEstimatorId_, keySegnumEstimatorId_;
-            unsigned int deltaParitySegnumEstimatorId_, keyParitySegnumEstimatorId_;
-            int64_t encodingTimestampMs_;
-            bool encodeFinished_;
-            IVideoThreadCallback *callback_;
+            VideoThread(const VideoThread&) = delete;
+            VideoCoder coder_;
+            unsigned int nEncoded_, nDropped_;
             
-            Name deltaFramesPrefix_, keyFramesPrefix_;
-            boost::shared_ptr<VideoCoder> coder_;
+            #warning using shared pointer here as libstdc++ on OSX does not support std::move
+            // TODO: update code to use std::move on Ubuntu
+            boost::shared_ptr<VideoFramePacket> videoFramePacket_;
             
-            boost::mutex frameProcessingMutex_;
-            WebRtcVideoFrame deliveredFrame_;
-            
-            void
-            onInterest(const boost::shared_ptr<const Name>& prefix,
-                       const boost::shared_ptr<const Interest>& interest,
-                       ndn::Face& face,
-                       uint64_t ts,
-                       const boost::shared_ptr<const InterestFilter>& filter);
-            
-            int
-            publishParityData(PacketNumber frameNo,
-                              const PacketData& packetData,
-                              unsigned int nSegments,
-                              const Name& framePrefix,
-                              const PrefixMetaInfo& prefixMeta);
-            
-            // interface conformance
             void
             onEncodingStarted();
             
             void
-            onEncodedFrameDelivered(const webrtc::EncodedImage &encodedImage,
-                                    double captureTimestamp,
-                                    bool completeFrame);
-            
+            onEncodedFrame(const webrtc::EncodedImage &encodedImage);
+
             void
-            onFrameDropped();
-            
-            void
-            publishFrameData(const webrtc::EncodedImage &encodedImage,
-                             double captureTimestamp,
-                             bool completeFrame);
-            
-            VideoThreadSettings&
-            getSettings() const
-            { return *((VideoThreadSettings*)settings_); }
+            onDroppedFrame();
         };
     }
 }
