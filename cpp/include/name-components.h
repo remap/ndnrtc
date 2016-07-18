7d4e2c2fb13aaed09de9536290bc6ea2b27afaac
peetonn <gpeetonn@gmail.com>
2016-07-18 15:05:09 -0700

    added helper macro to name components

--- cpp/include/name-components.h
+++ cpp/include/name-components.h
@@ -17,6 +17,9 @@
 #include "params.h"
 #include "ndnrtc-common.h"
 
+#define SAMPLE_SUFFIX(n)(n.getSubName(-3,3))
+#define PARITY_SUFFIX(n)(n.getSubName(-4,4))
+
 namespace ndnrtc {
     namespace prefix_filter {
         typedef enum _PrefixFilter {
@@ -78,49 +81,6 @@ namespace ndnrtc {
 
     class NameComponents {
     public:
-        #if 0
-        static const std::string NameComponentApp;
-        static const std::string NameComponentUser;
-        static const std::string NameComponentBroadcast;
-        static const std::string NameComponentDiscovery;
-        static const std::string NameComponentUserStreams;
-        static const std::string NameComponentSession;
-        static const std::string NameComponentStreamAccess;
-        static const std::string NameComponentStreamKey;
-        static const std::string NameComponentStreamFramesDelta;
-        static const std::string NameComponentStreamFramesKey;
-        static const std::string NameComponentStreamInfo;
-        static const std::string NameComponentFrameSegmentData;
-        static const std::string NameComponentFrameSegmentParity;
-        static const std::string KeyComponent;
-        static const std::string CertificateComponent;
-        
-        static std::string
-        getUserPrefix(const std::string& username,
-                      const std::string& prefix);
-        
-        static std::string
-        getStreamPrefix(const std::string& streamName,
-                        const std::string& username,
-                        const std::string& prefix);
-        
-        static std::string
-        getThreadPrefix(const std::string& threadName,
-                        const std::string& streamName,
-                        const std::string& username,
-                        const std::string& prefix);
-
-        static std::string 
-        getUserName(const std::string& prefix);
-
-        static std::string 
-        getStreamName(const std::string& prefix);
-
-        static std::string 
-        getThreadName(const std::string& prefix);
-        #endif
-
-        //****************************************************************************
         static const std::string NameComponentApp;
         static const std::string NameComponentAudio;
         static const std::string NameComponentVideo;
