f50cc0a41557ad9283c6bd99064c029c6bd11cd2
peetonn <gpeetonn@gmail.com>
2016-07-18 15:23:33 -0700

    updated IBuffer mock interface

--- cpp/tests/mock-objects/buffer-mock.h
+++ cpp/tests/mock-objects/buffer-mock.h
@@ -20,6 +20,9 @@ public:
 	MOCK_METHOD1(received, ndnrtc::BufferReceipt(const boost::shared_ptr<ndnrtc::WireSegment>&));
 	MOCK_CONST_METHOD1(isRequested, bool(const boost::shared_ptr<ndnrtc::WireSegment>&));
 	MOCK_CONST_METHOD2(getSlotsNum, unsigned int(const ndn::Name&, int));
+    MOCK_CONST_METHOD0(shortdump, std::string());
+    MOCK_METHOD1(attach, void(ndnrtc::IBufferObserver));
+    MOCK_METHOD1(detach, void(ndnrtc::IBufferObserver));
 };
 
 #endif
