74e57098472dfeab22dfc92299fa17466c1117e4
peetonn <gpeetonn@gmail.com>
2016-07-18 15:08:13 -0700

    fixed calculating DRD and added checks for observers

--- cpp/src/buffer-control.cpp
+++ cpp/src/buffer-control.cpp
@@ -25,13 +25,14 @@ BufferControl::~BufferControl()
 void 
 BufferControl::attach(IBufferControlObserver* o)
 {
-	observers_.push_back(o);
+	if (o) observers_.push_back(o);
 }
 
 void 
 BufferControl::detach(IBufferControlObserver* o)
 {
-	observers_.erase(std::find(observers_.begin(), observers_.end(), o));
+    std::vector<IBufferControlObserver*>::iterator it  = std::find(observers_.begin(), observers_.end(), o);
+    if (it != observers_.end()) observers_.erase(it);
 }
 
 void
@@ -40,7 +41,7 @@ BufferControl::segmentArrived(const boost::shared_ptr<WireSegment>& segment)
 	if (buffer_->isRequested(segment))
 	{
 		BufferReceipt receipt = buffer_->received(segment);
-		drdEstimator_->newValue(receipt.segment_->getRoundTripDelayUsec()/1000, receipt.segment_->isOriginal());
+		drdEstimator_->newValue(receipt.segment_->getDrdUsec()/1000, receipt.segment_->isOriginal());
 
 		if (segment->isPacketHeaderSegment())
 			for (auto& o:observers_) o->targetRateUpdate(receipt.segment_->getData()->packetHeader().sampleRate_);
@@ -48,11 +49,10 @@ BufferControl::segmentArrived(const boost::shared_ptr<WireSegment>& segment)
 		if (receipt.slot_->getFetchedNum() == 1)
 			for (auto& o:observers_) o->sampleArrived(segment->getPlaybackNo());
 
-		LogTraceC << "added segment " << receipt.segment_->getInfo().getSuffix(suffix_filter::Stream) 
-			<< (receipt.segment_->isOriginal() ? "ORIG" : "CACH")
-			<< ". DRD: original - " << drdEstimator_->getOriginalEstimation() 
-			<< " cached - " << drdEstimator_->getCachedEstimation() << std::endl;
+		LogDebugC << "added segment " << receipt.segment_->getInfo().getSuffix(suffix_filter::Thread)
+			<< (receipt.segment_->isOriginal() ? " ORIG" : " CACH")
+            << std::endl;
 	}
 	else
-		LogWarnC << "received unwanted/removed(late) data " << segment->getData()->getName() << std::endl;
+		LogWarnC << "received data is not in the buffer: " << segment->getData()->getName() << std::endl;
 }
