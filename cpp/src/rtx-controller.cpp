e419ad6b2078a3b33999b81a4b04deacb42bbb50
peetonn <gpeetonn@gmail.com>
2018-04-16 15:37:48 -0300

    [code formatting]

--- cpp/src/rtx-controller.cpp
+++ cpp/src/rtx-controller.cpp
@@ -21,87 +21,87 @@ using namespace ndnrtc::statistics;
 #define RTX_DEADLINE_MS 100
 
 RetransmissionController::RetransmissionController(boost::shared_ptr<statistics::StatisticsStorage> storage,
-	boost::shared_ptr<IPlaybackQueue> playbackQueue,
-    const boost::shared_ptr<DrdEstimator> &drdEstimator)
-  :StatObject(storage), 
-  playbackQueue_(playbackQueue),
-  drdEstimator_(drdEstimator),
-  enabled_(false)
+                                                   boost::shared_ptr<IPlaybackQueue> playbackQueue,
+                                                   const boost::shared_ptr<DrdEstimator> &drdEstimator)
+    : StatObject(storage),
+      playbackQueue_(playbackQueue),
+      drdEstimator_(drdEstimator),
+      enabled_(false)
 {
-	description_ = "rtx-controller";
+    description_ = "rtx-controller";
 }
 
-void RetransmissionController::attach(IRtxObserver* observer)
+void RetransmissionController::attach(IRtxObserver *observer)
 {
-	if (observer)
-		observers_.push_back(observer);
+    if (observer)
+        observers_.push_back(observer);
 }
 
-void RetransmissionController::detach(IRtxObserver* observer)
+void RetransmissionController::detach(IRtxObserver *observer)
 {
-	std::vector<IRtxObserver*>::iterator it = std::find(observers_.begin(), observers_.end(), observer);
+    std::vector<IRtxObserver *>::iterator it = std::find(observers_.begin(), observers_.end(), observer);
 
-	if (it != observers_.end())
-		observers_.erase(it);
+    if (it != observers_.end())
+        observers_.erase(it);
 }
 
-void RetransmissionController::onNewRequest(const boost::shared_ptr<BufferSlot>& slot)
+void RetransmissionController::onNewRequest(const boost::shared_ptr<BufferSlot> &slot)
 {
-    if (!enabled_) return;
+    if (!enabled_)
+        return;
 
-	if (activeSlots_.find(slot->getPrefix()) != activeSlots_.end())
-		throw std::runtime_error("slot is being tracked already");
+    if (activeSlots_.find(slot->getPrefix()) != activeSlots_.end())
+        throw std::runtime_error("slot is being tracked already");
 
-	int64_t now = clock::millisecondTimestamp();
-	int64_t queueSize = playbackQueue_->size()+playbackQueue_->pendingSize();
-	// for key frames playback delay will be GOP milliseconds from now
-	// NOTE: gop is assumed as 30 below. probably need to be changed to adequate number
-	int64_t playbackDeadline = (slot->getNameInfo().class_ == SampleClass::Key ? 
-			now + playbackQueue_->samplePeriod()*30 : now+queueSize);
+    int64_t now = clock::millisecondTimestamp();
+    int64_t queueSize = playbackQueue_->size() + playbackQueue_->pendingSize();
+    // for key frames playback delay will be GOP milliseconds from now
+    // NOTE: gop is assumed as 30 below. probably need to be changed to adequate number
+    int64_t playbackDeadline = (slot->getNameInfo().class_ == SampleClass::Key ? now + playbackQueue_->samplePeriod() * 30 : now + queueSize);
 
-	activeSlots_[slot->getPrefix()] = { slot, playbackDeadline };
+    activeSlots_[slot->getPrefix()] = {slot, playbackDeadline};
 
-	checkRetransmissions();
+    checkRetransmissions();
 }
 
-void RetransmissionController::onNewData(const BufferReceipt& receipt)
+void RetransmissionController::onNewData(const BufferReceipt &receipt)
 {
     if (enabled_)
-	    checkRetransmissions();
+        checkRetransmissions();
 }
 
 void RetransmissionController::onReset()
 {
-	activeSlots_.clear();
+    activeSlots_.clear();
 }
 
 void RetransmissionController::checkRetransmissions()
 {
-	int64_t now = clock::millisecondTimestamp();
+    int64_t now = clock::millisecondTimestamp();
 
-	for (auto it = activeSlots_.begin(); it != activeSlots_.end(); /* no increment */)
-	{
-		boost::shared_ptr<BufferSlot> slot = it->second.slot_;
-		int64_t playbackDeadline = it->second.deadlineTimestamp_;
-		bool needRtx = (playbackDeadline-now < drdEstimator_->getOriginalAverage().latestValue());
-		bool assembledOrCleared = (slot->getState() >= BufferSlot::State::Ready || slot->getState() == BufferSlot::State::Free);
+    for (auto it = activeSlots_.begin(); it != activeSlots_.end(); /* no increment */)
+    {
+        boost::shared_ptr<BufferSlot> slot = it->second.slot_;
+        int64_t playbackDeadline = it->second.deadlineTimestamp_;
+        bool needRtx = (playbackDeadline - now < drdEstimator_->getOriginalAverage().latestValue());
+        bool assembledOrCleared = (slot->getState() >= BufferSlot::State::Ready || slot->getState() == BufferSlot::State::Free);
 
-		if (needRtx || assembledOrCleared)
-		{
-			activeSlots_.erase(it++);
+        if (needRtx || assembledOrCleared)
+        {
+            activeSlots_.erase(it++);
 
-			if (needRtx && !assembledOrCleared)
-			{
-				LogTraceC << "rtx required " << slot->dump() 
-					<< " playback in " << playbackDeadline-now << "ms" << std::endl;
+            if (needRtx && !assembledOrCleared)
+            {
+                LogTraceC << "rtx required " << slot->dump()
+                          << " playback in " << playbackDeadline - now << "ms" << std::endl;
 
-				std::vector<boost::shared_ptr<const ndn::Interest>> pendingInterests = slot->getPendingInterests();
-				if (pendingInterests.size())
-					for (auto o:observers_)
-						o->onRetransmissionRequired(pendingInterests);
-			}
-		}
-		else
-			++it;
-	}
+                std::vector<boost::shared_ptr<const ndn::Interest>> pendingInterests = slot->getPendingInterests();
+                if (pendingInterests.size())
+                    for (auto o : observers_)
+                        o->onRetransmissionRequired(pendingInterests);
+            }
+        }
+        else
+            ++it;
+    }
 }
--- cpp/src/rtx-controller.hpp
+++ cpp/src/rtx-controller.hpp
@@ -15,51 +15,53 @@
 #include "frame-buffer.hpp"
 #include "statistics.hpp"
 
-namespace ndnrtc {
-	class IPlaybackQueue;
-	class IRtxObserver;
-    class DrdEstimator;
+namespace ndnrtc
+{
+class IPlaybackQueue;
+class IRtxObserver;
+class DrdEstimator;
 
-	class RetransmissionController : public NdnRtcComponent, 
-									 public IBufferObserver,
-									 public statistics::StatObject
-	{
-	public:
-		RetransmissionController(boost::shared_ptr<statistics::StatisticsStorage> storage,
-			boost::shared_ptr<IPlaybackQueue> playbackQueue,
-            const boost::shared_ptr<DrdEstimator> &drdEstimator);
+class RetransmissionController : public NdnRtcComponent,
+                                 public IBufferObserver,
+                                 public statistics::StatObject
+{
+  public:
+    RetransmissionController(boost::shared_ptr<statistics::StatisticsStorage> storage,
+                             boost::shared_ptr<IPlaybackQueue> playbackQueue,
+                             const boost::shared_ptr<DrdEstimator> &drdEstimator);
 
+    void attach(IRtxObserver *observer);
+    void detach(IRtxObserver *observer);
 
-		void attach(IRtxObserver* observer);
-		void detach(IRtxObserver* observer);
+    void setEnabled(bool enable) { enabled_ = enable; }
+    bool isEnabled() { return enabled_; }
 
-        void setEnabled(bool enable) { enabled_ = enable; }
-        bool isEnabled() { return enabled_; }
+  private:
+    typedef struct _ActiveSlotListEntry
+    {
+        boost::shared_ptr<BufferSlot> slot_;
+        int64_t deadlineTimestamp_;
+    } ActiveSlotListEntry;
 
-	private:
-		typedef struct _ActiveSlotListEntry {
-			boost::shared_ptr<BufferSlot> slot_;
-			int64_t deadlineTimestamp_;
-		} ActiveSlotListEntry;
+    std::vector<IRtxObserver *> observers_;
+    std::map<ndn::Name, ActiveSlotListEntry> activeSlots_;
+    boost::shared_ptr<IPlaybackQueue> playbackQueue_;
+    boost::shared_ptr<DrdEstimator> drdEstimator_;
+    bool enabled_;
 
-		std::vector<IRtxObserver*> observers_;
-		std::map<ndn::Name,  ActiveSlotListEntry> activeSlots_;
-		boost::shared_ptr<IPlaybackQueue> playbackQueue_;
-        boost::shared_ptr<DrdEstimator> drdEstimator_;
-        bool enabled_;
+    void checkRetransmissions();
 
-		void checkRetransmissions();
+    // IBuffer observer
+    void onNewRequest(const boost::shared_ptr<BufferSlot> &);
+    void onNewData(const BufferReceipt &receipt);
+    void onReset();
+};
 
-        // IBuffer observer
-        void onNewRequest(const boost::shared_ptr<BufferSlot>&);
-        void onNewData(const BufferReceipt& receipt);
-        void onReset();
-	};
-
-	class IRtxObserver {
-	public:
-		virtual void onRetransmissionRequired(const std::vector<boost::shared_ptr<const ndn::Interest>>&) = 0;
-	};
+class IRtxObserver
+{
+  public:
+    virtual void onRetransmissionRequired(const std::vector<boost::shared_ptr<const ndn::Interest>> &) = 0;
+};
 }
 
 #endif
