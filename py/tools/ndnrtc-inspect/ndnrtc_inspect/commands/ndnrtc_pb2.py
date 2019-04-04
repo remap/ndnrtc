3304ddfa202b193bc07002b33bb3ebda68113f36
peetonn <gpeetonn@gmail.com>
2019-04-04 12:57:10 -0700

    updated protobuf description for live metadata

--- py/tools/ndnrtc-inspect/ndnrtc_inspect/commands/ndnrtc_pb2.py
+++ py/tools/ndnrtc-inspect/ndnrtc_inspect/commands/ndnrtc_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='ndnrtc',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x0cndnrtc.proto\x12\x06ndnrtc\x1a\x1fgoogle/protobuf/timestamp.proto\"c\n\nStreamMeta\x12\r\n\x05width\x18\x01 \x01(\r\x12\x0e\n\x06height\x18\x02 \x01(\r\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x0f\n\x07\x62itrate\x18\x04 \x01(\r\x12\x10\n\x08gop_size\x18\x05 \x01(\r\"\xae\x01\n\x08LiveMeta\x12-\n\ttimestamp\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x11\n\tframerate\x18\x02 \x01(\x02\x12\x14\n\x0csegnum_delta\x18\x03 \x01(\x02\x12\x12\n\nsegnum_key\x18\x04 \x01(\x02\x12\x1b\n\x13segnum_delta_parity\x18\x05 \x01(\x02\x12\x19\n\x11segnum_key_parity\x18\x06 \x01(\x02\"\xf4\x01\n\tFrameMeta\x12\x13\n\x0bparity_size\x18\x01 \x01(\x05\x12\x14\n\x0cgop_position\x18\x02 \x01(\x05\x12\x12\n\ngop_number\x18\x03 \x01(\x05\x12\x35\n\x11\x63\x61pture_timestamp\x18\x04 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x18\n\x10generation_delay\x18\x05 \x01(\x05\x12)\n\x04type\x18\x06 \x01(\x0e\x32\x1b.ndnrtc.FrameMeta.FrameType\",\n\tFrameType\x12\x0b\n\x07Unknown\x10\x00\x12\t\n\x05\x44\x65lta\x10\x01\x12\x07\n\x03Key\x10\x02\x62\x06proto3')
+  serialized_pb=_b('\n\x0cndnrtc.proto\x12\x06ndnrtc\x1a\x1fgoogle/protobuf/timestamp.proto\"c\n\nStreamMeta\x12\r\n\x05width\x18\x01 \x01(\r\x12\x0e\n\x06height\x18\x02 \x01(\r\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x0f\n\x07\x62itrate\x18\x04 \x01(\r\x12\x10\n\x08gop_size\x18\x05 \x01(\r\"\xe3\x01\n\x08LiveMeta\x12-\n\ttimestamp\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x11\n\tframerate\x18\x02 \x01(\x02\x12\x14\n\x0csegnum_delta\x18\x03 \x01(\x02\x12\x12\n\nsegnum_key\x18\x04 \x01(\x02\x12\x1b\n\x13segnum_delta_parity\x18\x05 \x01(\x02\x12\x19\n\x11segnum_key_parity\x18\x06 \x01(\x02\x12\x17\n\x0fsegnum_estimate\x18\x07 \x01(\x05\x12\x1a\n\x12\x66ramesize_estimate\x18\x08 \x01(\x05\"\x89\x02\n\tFrameMeta\x12\x13\n\x0bparity_size\x18\x01 \x01(\x05\x12\x14\n\x0cgop_position\x18\x02 \x01(\x05\x12\x12\n\ngop_number\x18\x03 \x01(\x05\x12\x35\n\x11\x63\x61pture_timestamp\x18\x04 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x18\n\x10generation_delay\x18\x05 \x01(\x05\x12)\n\x04type\x18\x06 \x01(\x0e\x32\x1b.ndnrtc.FrameMeta.FrameType\x12\x13\n\x0b\x64\x61taseg_num\x18\x07 \x01(\x05\",\n\tFrameType\x12\x0b\n\x07Unknown\x10\x00\x12\t\n\x05\x44\x65lta\x10\x01\x12\x07\n\x03Key\x10\x02\x62\x06proto3')
   ,
   dependencies=[google_dot_protobuf_dot_timestamp__pb2.DESCRIPTOR,])
 
@@ -47,8 +47,8 @@ _FRAMEMETA_FRAMETYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=536,
-  serialized_end=580,
+  serialized_start=610,
+  serialized_end=654,
 )
 _sym_db.RegisterEnumDescriptor(_FRAMEMETA_FRAMETYPE)
 
@@ -161,6 +161,20 @@ _LIVEMETA = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='segnum_estimate', full_name='ndnrtc.LiveMeta.segnum_estimate', index=6,
+      number=7, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='framesize_estimate', full_name='ndnrtc.LiveMeta.framesize_estimate', index=7,
+      number=8, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -174,7 +188,7 @@ _LIVEMETA = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=159,
-  serialized_end=333,
+  serialized_end=386,
 )
 
 
@@ -227,6 +241,13 @@ _FRAMEMETA = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='dataseg_num', full_name='ndnrtc.FrameMeta.dataseg_num', index=6,
+      number=7, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -240,8 +261,8 @@ _FRAMEMETA = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=336,
-  serialized_end=580,
+  serialized_start=389,
+  serialized_end=654,
 )
 
 _LIVEMETA.fields_by_name['timestamp'].message_type = google_dot_protobuf_dot_timestamp__pb2._TIMESTAMP
