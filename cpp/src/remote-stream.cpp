bd2c556315d45a454babf7a1f90fd210f8e30b1b
peetonn <gpeetonn@gmail.com>
2019-06-13 10:27:26 -0700

    RemoteStream::getPrefix() fixed

--- cpp/src/remote-stream.cpp
+++ cpp/src/remote-stream.cpp
@@ -123,8 +123,8 @@ RemoteAudioStream::RemoteAudioStream(boost::asio::io_service& faceIo,
 			const int jitterSizeMs):
 RemoteStream(faceIo, face, keyChain, basePrefix, streamName)
 {
-	pimpl_ = boost::make_shared<RemoteAudioStreamImpl>(faceIo, face, keyChain, 
-                                                       NameComponents::audioStreamPrefix(basePrefix).append(streamName).toUri());
+	streamPrefix_ = NameComponents::audioStreamPrefix(basePrefix).append(streamName).toUri();
+	pimpl_ = boost::make_shared<RemoteAudioStreamImpl>(faceIo, face, keyChain, streamPrefix_);
 	pimpl_->setInterestLifetime(interestLifetime);
 	pimpl_->setTargetBufferSize(jitterSizeMs);
 	pimpl_->fetchMeta();
@@ -146,8 +146,9 @@ RemoteVideoStream::RemoteVideoStream(boost::asio::io_service& faceIo,
 			const int jitterSizeMs):
 RemoteStream(faceIo, face, keyChain, basePrefix, streamName)
 {
-	pimpl_ = boost::make_shared<RemoteVideoStreamImpl>(faceIo, face, keyChain,
-                                                      NameComponents::videoStreamPrefix(basePrefix).append(streamName).toUri());
+	streamPrefix_ = NameComponents::videoStreamPrefix(basePrefix).append(streamName).toUri();
+	pimpl_ = boost::make_shared<RemoteVideoStreamImpl>(faceIo, face, keyChain, streamPrefix_);
+
 	pimpl_->setInterestLifetime(interestLifetime);
 	pimpl_->setTargetBufferSize(jitterSizeMs);
 	pimpl_->fetchMeta();
