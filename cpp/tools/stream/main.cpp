c15b6f06b23376841838603e9da0cf2eb6a3b84c
peetonn <gpeetonn@gmail.com>
2019-08-20 00:02:46 -0700

    removed debug output

--- cpp/tools/stream/main.cpp
+++ cpp/tools/stream/main.cpp
@@ -172,8 +172,8 @@ int main(int argc, char **argv)
                          true,               // show help if requested
                          (string(TOOL_NAME)+string(PACKAGE_VERSION)).c_str());  // version string
 
-     for(auto const& arg : args)
-         std::cout << arg.first << " " <<  arg.second << std::endl;
+     // for(auto const& arg : args)
+     //     std::cout << arg.first << " " <<  arg.second << std::endl;
 
     AppLog = args["--log"].asString();
     cout << AppLog << endl;
